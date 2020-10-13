24a212bb4962e7fc63fd9d23ad734d94cb22a16a
hunter Jozwiak <hunter.t.joz@gmail.com>
2020-10-13 13:16:01 -0400

    Set up the path so that we can load our modules.

---
+++ start.lua
@@ -0,0 +1,10 @@
+-- The main launchpoint for the package.
+-- Responsible for loading modules, welcoming the user, and establishing a connection to the MUD.
+
+-- Set up the path for the modules.
+package.path = os.getenv("PWD") .. "src/?.lua" .. path
+
+-- Load in the modules.
+require config
+
+blight:output("Welcome to the StickMUD experience for BlightMUD! Please type login to play.")
