d3ed93d08e2eae0ff1257b24d98dce1edcbf1067
hunter Jozwiak <hunter.t.joz@gmail.com>
2020-10-13 23:42:19 -0400

    Load in the sondplayer for testing.

--- start.lua
+++ start.lua
@@ -1,11 +1,10 @@
 -- The main launchpoint for the package.
 -- Responsible for loading modules, welcoming the user, and establishing a connection to the MUD.
-
 -- Set up the path for the modules.
 package.path = os.getenv("PWD") .. "/src/?.lua;" .. package.path
 
 -- Load in the modules.
 require("config")
-require("soundplayer")
+sp = require("soundplayer")
 
 blight:output("Welcome to the StickMUD experience for BlightMUD! Please type login to play.")
