fb6c9bc71880c3a0de598b612a57899c6d7934ae
hunter Jozwiak <hunter.t.joz@gmail.com>
2020-10-14 15:21:24 -0400

    Initial configurations.

---
+++ src/config.lua
@@ -0,0 +1,4 @@
+-- Configuration for the package.
+local config = {}
+config.tls = true -- Should we use TLS when playing the game?
+config.gmcp_modules = {"Client.Media", "Char", "Room"} -- The GMCP modules to register
