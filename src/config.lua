4ab2f0ea7d2a43a1f0f3118c4bba981790f6a771
hunter Jozwiak <hunter.t.joz@gmail.com>
2020-10-14 15:38:12 -0400

    Factor in the host and port as well.

--- src/config.lua
+++ src/config.lua
@@ -2,3 +2,4 @@
 local config = {}
 config.tls = true -- Should we use TLS when playing the game?
 config.gmcp_modules = {"Client.Media", "Char", "Room"} -- The GMCP modules to register
+return config
