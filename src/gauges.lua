be98485ac2cc79a523c8e64f23acde9bb07139fb
hunter Jozwiak <hunter.t.joz@gmail.com>
2020-10-15 14:32:57 -0400

    Updated gauges code. The next task is to figure out why Char.Vitals isn't being updated.

--- src/gauges.lua
+++ src/gauges.lua
@@ -1,25 +1,48 @@
 local gauges = {}
+local oldhp = 0
+local oldsp = 0
+local oldfp = 0
 
-function gauges.round(n)
-    return n % 1 >= 0.5 and math.ceil(n) or math.floor(n)
+local function nearest_percentage(n)
+    return n - n % 10
 end
 
+local function do_health(n)
+    if n ~= oldhp then
+        oldhp = n
+        sp.play_var(sv.health[n])
+    end
+end
+
+local function do_fp(n)
+    if n ~= oldfp then
+        oldfp = n
+        sp.play_var(sv.fatigue[n])
+    end
+end
+
+local function do_sp(n)
+    if n ~= oldsp then
+        oldsp = n
+        sp.play_var(sv.spell[n])
+    end
+end
+
+-- The main processing function.
 local function process_vitals(data)
-    blight:output(data)
     local obj = json.decode(data)
-    local hit = obj["hp"]
-    local maxhit = obj["maxhp"]
-    local spell = obj["sp"]
-    local maxspell = obj["maxsp"]
-    local fat = obj["fp"]
-    local maxfat = obj["maxfp"]
-    local hit_percent = gauges.round((hit / maxhit) * 100)
-    local fatigue_percent = gauges.round((fat / maxfat) * 100)
-    local spell_percent = gauges.round((spell / maxspell) * 100)
-    sp.play_var(sv.health[hit_percent])
-    sp.play_var(sv.fatigue[fatigue_percent])
-    sp.play_var(sv.spell[spell_percent])
+    local hit = tonumber(obj["hp"])
+    local maxhit = tonumber(obj["maxhp"])
+    local fat = tonumber(obj["fp"])
+    local maxfat = tonumber(obj["maxfp"])
+    local spell = tonumber(obj["sp"])
+    local maxspell = tonumber(obj["maxsp"])
+    local hit_percent = string.format("%d", nearest_percentage((hit / maxhit) * 100))
+    local spell_percent = string.format("%d", nearest_percentage((spell / maxspell) * 100))
+    local fat_percent = string.format("%d", nearest_percentage((fat / maxfat) * 100))
+    do_health(hit_percent)
+    do_sp(spell_percent)
+    do_fp(fat_percent)
 end
-
 gmcp.receive("Char.Vitals", process_vitals)
 return gauges
