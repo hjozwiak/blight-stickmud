9e437f584d13bda1ca178e25f9dbf4f6aa0b4cb2
hunter Jozwiak <hunter.t.joz@gmail.com>
2020-12-01 20:27:07 -0500

    More fixes. Forgot to change blight:send to mud.send.

--- src/navigation.lua
+++ src/navigation.lua
@@ -1,26 +1,26 @@
 local navigation = {}
 
 blight:bind("alt-i", function()
-    blight:send("north", {
+    mud.send("north", {
         gag = true
     })
 end)
 
-blight:bind("alt-l", function() blight:send("east", {gag = true}) end)
+blight:bind("alt-l", function() mud.send("east", {gag = true}) end)
 
-blight:bind("alt-j", function() blight:send("west", {gag = true}) end)
+blight:bind("alt-j", function() mud.send("west", {gag = true}) end)
 
-blight:bind("alt-,", function() blight:send("south", {gag = true}) end)
+blight:bind("alt-,", function() mud.send("south", {gag = true}) end)
 
-blight:bind("alt-o", function() blight:send("northeast", {gag = true}) end)
+blight:bind("alt-o", function() mud.send("northeast", {gag = true}) end)
 
-blight:bind("alt-u", function() blight:send("northwest", {gag = true}) end)
+blight:bind("alt-u", function() mud.send("northwest", {gag = true}) end)
 
-blight:bind("alt-.", function() blight:send("southeast", {gag = true}) end)
+blight:bind("alt-.", function() mud.send("southeast", {gag = true}) end)
 
-blight:bind("alt-m", function() blight:send("southwest", {gag = true}) end)
+blight:bind("alt-m", function() mud.send("southwest", {gag = true}) end)
 
-blight:bind("alt-[", function() blight:send("up", {gag = true}) end)
+blight:bind("alt-[", function() mud.send("up", {gag = true}) end)
 
-blight:bind("alt-]", function() blight:send("down", {gag = true}) end)
+blight:bind("alt-]", function() mud.send("down", {gag = true}) end)
 return navigation
